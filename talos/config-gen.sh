a0b7875313872e890d363dcc7609b43d91191543
Nick Davies <git@nicolasdavies.com.au>
2025-06-18 14:55:40 -0600

    Improve config-gen.sh
    First make it so that we only envsubst variables that are provided to us.
    This means that if you have a patch with $delete in it it doesn't get
    trampled by the envsubst.
    
    Also split up config generation. talosctl will generate both the
    controlplane and worker configs internally even if you provide
    --output-types. This means that a patch (say a delete) that is only
    compatible with one of the configs will fail to apply even though that
    final config would never be outputted anyway.
    
    To avoid this we generate the whole config first and then apply all the
    patches on top.

--- talos/config-gen.sh
+++ talos/config-gen.sh
@@ -21,23 +21,35 @@ PATCHES=$(find ./patches | grep -e '.ya\?ml' | sed 's/^\.\///' | sort)
 OUTPUT_PATCHES_DIR="./output/rendered/patches"
 OUTPUT_NODES_DIR="output/rendered/nodes"
 
+rm -f "$OUTPUT_NODES_DIR/${NODE}.raw.yaml"
 rm -f "$OUTPUT_NODES_DIR/${NODE}.yaml"
 mkdir -p "$OUTPUT_NODES_DIR"
 mkdir -p "$OUTPUT_PATCHES_DIR"
 
+VALID_VARS=$(cat talosenv | grep -v "^\s*$" | sed 's/\([^=]*\)=.*/${\1}/')
+
 patchesArray=()
 for PATCH in $PATCHES; do
     mkdir -p "$OUTPUT_PATCHES_DIR/$(dirname $PATCH)"
-    cat "$PATCH" | envsubst > "$OUTPUT_PATCHES_DIR/$PATCH"
-    patchesArray+=(--config-patch @"$OUTPUT_PATCHES_DIR/$PATCH")
+    cat "$PATCH" | envsubst "$VALID_VARS" > "$OUTPUT_PATCHES_DIR/$PATCH"
+    patchesArray+=(--patch @"$OUTPUT_PATCHES_DIR/$PATCH")
 done
 
+# First generate a base config without any of the patches.
+# This lets us scope down to only generating the controlplane at this point
+# without having talosctl gen try and generate both (which may be incompatible with
+# certain patches)
 talosctl gen config "$CLUSTER_NAME" "$CLUSTER_ENDPOINT" \
     --with-secrets "$SECRETS_DIR/talos.yaml" \
     --with-examples=false \
     --with-docs=false \
     --install-disk "" \
     --output-types="controlplane" \
-    --output "$OUTPUT_NODES_DIR/${NODE}.yaml" \
+    --output "$OUTPUT_NODES_DIR/${NODE}.raw.yaml" \
+
+# Next we apply all our patches on top to make the final config
+talosctl machineconfig patch \
+    "$OUTPUT_NODES_DIR/${NODE}.raw.yaml" \
+    -o "$OUTPUT_NODES_DIR/${NODE}.yaml" \
     "${patchesArray[@]}" \
-    --config-patch "$(cat $NODE_PATCH | envsubst)"
+    --patch "$(cat $NODE_PATCH | envsubst "$VALID_VARS")"
