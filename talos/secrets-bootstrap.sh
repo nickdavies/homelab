500e711982c8d21830fb7ee6b5caad722ab80865
Nick Davies <git@nicolasdavies.com.au>
2025-06-19 12:05:44 -0600

    Add script to bootstrap flux and 1password secrets
    This is needed to turn up the whole cluster. Flux needs it's git keys to
    go and configure everything and 1password keys need to be injected so
    that onepassword-connect will function and provide all the ExternalSecrets
    that are declared in flux that are used by the rest of the cluster.

---
+++ talos/secrets-bootstrap.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -euo pipefail
+
+OUTPUT_DIR="./output/"
+SECRETS_DIR="./$OUTPUT_DIR/secrets/"
+MARKER_FILE="./$OUTPUT_DIR/.marker"
+
+set -a; source talosenv; set +a
+
+OP_CREDENTIALS_FILE="$SECRETS_DIR/1password-credentials.json"
+OP_TOKEN_FILE="$SECRETS_DIR/1password_access_token"
+FLUX_KEY_FILE="$SECRETS_DIR/deploy_key.key"
+
+if [ ! -f "$MARKER_FILE" ]; then
+    echo "Secrets appear not to be mounted please run load-secrets.sh first"
+    exit 1
+fi
+
+if [ ! -f "$OP_CREDENTIALS_FILE" ]; then
+    echo "1password connect credentials expected at $OP_CREDENTIALS_FILE but was not found"
+    exit 1
+fi
+if [ ! -f "$OP_TOKEN_FILE" ]; then
+    echo "1password connect token expected at $OP_TOKEN_FILE but was not found"
+    exit 1
+fi
+
+if [ ! -f "$FLUX_KEY_FILE" ]; then
+    echo "Flux private key expected at $FLUX_KEY_FILE but was not found"
+    exit 1
+fi
+
+if [ -z "$FLUX_REPO_URL" ]; then
+    echo "FLUX_REPO_URL unset make sure this is configured in talosenv"
+    exit 1
+fi
+
+OP_CREDENTIALS=$(cat $OP_CREDENTIALS_FILE | base64 | tr '/+' '_-' | tr -d '=' | tr -d '\n')
+OP_TOKEN=$(cat $OP_TOKEN_FILE)
+
+flux create secret git homelab-auth \
+    --export \
+    --url "$FLUX_REPO_URL" \
+    --private-key-file $FLUX_KEY_FILE \
+    | kubectl apply -f -
+
+kubectl create secret generic onepassword-connect-credentials \
+    --from-literal="1password-credentials.json=$OP_CREDENTIALS" \
+    -n external-secrets \
+    --dry-run=client \
+    -o yaml | kubectl apply -f -
+
+kubectl create secret generic onepassword-connect-token \
+    --from-literal="token=$OP_TOKEN" \
+    -n external-secrets \
+    --dry-run=client \
+    -o yaml \
+    | kubectl apply -f -
