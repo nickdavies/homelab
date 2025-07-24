311433d2c9117891426a689b20de71618578875a
Nick Davies <git@nicolasdavies.com.au>
2025-07-23 21:24:42 -0600

    Add coral drivers

--- talos/image_build/remote-image-gen.sh
+++ talos/image_build/remote-image-gen.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 FILE="$1"
+VERSION="${2:-<VERSION>}"
 
 # Check if customizations.yaml exists
 if [[ ! -f "$FILE" ]]; then
@@ -10,10 +11,10 @@ if [[ ! -f "$FILE" ]]; then
     exit 1
 fi
 
-OUTPUT=$(curl -X POST --data-binary @customizations.yaml https://factory.talos.dev/schematics)
+OUTPUT=$(curl -X POST --data-binary @${FILE} https://factory.talos.dev/schematics)
 echo $OUTPUT
 
-ID="$(echo $OUTPUT | yq '.id')"
+ID="$(echo $OUTPUT | yq '.id' -r)"
 
-echo "https://factory.talos.dev/image/${ID}/<VERSION>/metal-amd64.iso"
-echo "https://factory.talos.dev/image/${ID}/<VERSION>/metal-arm64.raw.xz"
+echo "https://factory.talos.dev/image/${ID}/${VERSION}/metal-amd64.iso"
+echo "https://factory.talos.dev/image/${ID}/${VERSION}/metal-arm64.raw.xz"
